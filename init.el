fb29f4ec4f7fbfaa1fec45adbb8a2fa192e122ea
sktoiva <sampo.toiva@gmail.com>
2019-08-02 17:09:55 +0300

    Remove tabs, setup whitespace mode

--- init.el
+++ init.el
@@ -1,23 +1,25 @@
 ;; ********************************************************************************************************************
 ;; Basic configuration settings and use package configuration
 
-(setq delete-old-versions -1		; delete excess backup versions silently
-      version-control t 		; use version control
-      vc-make-backup-files t 		; make backups file even when in version controlled dir
+(setq delete-old-versions -1    ; delete excess backup versions silently
+      version-control t         ; use version control
+      vc-make-backup-files t    ; make backups file even when in version controlled dir
       backup-directory-alist `(("." . "~/.emacs.d/backups"))  ; which directory to put backups file
-      vc-follow-symlinks t 				       ; don't ask for confirmation when opening symlinked file
+      vc-follow-symlinks t                ; don't ask for confirmation when opening symlinked file
       auto-save-file-name-transforms '((".*" "~/.emacs.d/auto-save-list/" t))  ;transform backups file name
       inhibit-splash-screen t
       inhibit-startup-echo-area-message t
       inhibit-startup-message t
-      ring-bell-function 'ignore 	; silent bell when you make a mistake
-      coding-system-for-read 'utf-8 	; use utf-8 by default
-      coding-system-for-write 'utf-8 
-      sentence-end-double-space nil	; sentence SHOULD end with only a point.
-      default-fill-column 80		; toggle wrapping text at the 80th character
+      ring-bell-function 'ignore   ; silent bell when you make a mistake
+      coding-system-for-read 'utf-8   ; use utf-8 by default
+      coding-system-for-write 'utf-8
+      sentence-end-double-space nil  ; sentence SHOULD end with only a point.
+      default-fill-column 80    ; toggle wrapping text at the 80th character
       windmove-wrap-around t            ; wrap aronud when navigating between windows
       custom-file "./custom.el")
 (load custom-file)
+
+(setq-default indent-tabs-mode nil)
 
 ;; Load files when changed on disk
 (global-auto-revert-mode t)
@@ -32,9 +34,15 @@
 (visual-line-mode 1)
 (global-hl-line-mode)
 
+;; Whitespace settings
+(setq whitespace-style '(face tabs tab-mark trailing)
+      mode-require-final-newline t)
+(global-whitespace-mode 1)
+(add-hook 'before-save-hook 'whetspace-cleanup)
+
 ;; Configure font
 (add-to-list 'default-frame-alist
-	     '(font . "Office Code Pro-13:weight=normal"))
+             '(font . "Office Code Pro-13:weight=normal"))
 ;; Window management
 (defadvice yes-or-no-p (around prevent-dialog activate)
   "Prevent yes-or-no-p from activating a dialog"
@@ -59,17 +67,17 @@
 ;; the following lines tell emacs where on the internet to look up
 ;; for new packages.
 (setq package-archives '(("org"       . "http://orgmode.org/elpa/")
-			 ("gnu"       . "http://elpa.gnu.org/packages/")
-			 ("melpa"     . "https://melpa.org/packages/")
-			 ("marmalade" . "http://marmalade-repo.org/packages/")))
-(package-initialize) 
+                         ("gnu"       . "http://elpa.gnu.org/packages/")
+                         ("melpa"     . "https://melpa.org/packages/")
+                         ("marmalade" . "http://marmalade-repo.org/packages/")))
+(package-initialize)
 
 ;; Bootstrap `use-package'
 (unless (package-installed-p 'use-package)
   (package-refresh-contents) ; upgrage packages archive
   (package-install 'use-package)) ; and install the most recent version of use-package
 
-(require 'use-package) 
+(require 'use-package)
 
 ;; ********************************************************************************************************************
 
@@ -90,8 +98,8 @@
   (general-evil-setup t) ;Generate vim like setters for general, like nmap)
   ;; Global prefix key definitions spanning multiple packages
   (nmap
-   :prefix "SPC"
-   "t" '(nil :which-key "toggle")))
+    :prefix "SPC"
+    "t" '(nil :which-key "toggle")))
 
 (use-package which-key
   :ensure t
@@ -99,7 +107,7 @@
   (which-key-mode)
   (which-key-setup-minibuffer))
 
-(use-package evil 
+(use-package evil
   :ensure t
   :config
   (evil-mode 1))
@@ -109,19 +117,19 @@
   :general
   ("M-x" #'helm-M-x)
   (:keymaps 'helm-map
-	    "<tab>" #'helm-execute-persistent-action
-	    "C-i" #'helm-execute-persistent-action)
+            "<tab>" #'helm-execute-persistent-action
+            "C-i" #'helm-execute-persistent-action)
 
   (nmap
     :prefix "SPC"
     "SPC" '(helm-find-files :which-key "helm find files"))
-  
+
   :init
   (setq helm-move-to-line-cycle-in-source     t ; move to end or beginning of source when reaching top or bottom of source.
-	helm-ff-file-name-history-use-recentf t
-	helm-mode-fuzzy-match                 t
-	helm-completion-in-region-fuzzy-match t
-	helm-M-x-fuzzy-match                  t)
+        helm-ff-file-name-history-use-recentf t
+        helm-mode-fuzzy-match                 t
+        helm-completion-in-region-fuzzy-match t
+        helm-M-x-fuzzy-match                  t)
 
   :config
   (require 'helm-config)
@@ -138,7 +146,7 @@
 (use-package smartparens
   :ensure t
   :hook ((prog-mode . smartparens-strict-mode)
-	 (prog-mode . show-smartparens-mode))
+         (prog-mode . show-smartparens-mode))
   :general
   (nmap
     :prefix "SPC"
@@ -147,7 +155,7 @@
     "sj" '(sp-splice-sexp-killing-backward :which-key "Splice killing backward"))
   :config
   (require 'smartparens-config)
-  
+
   (sp-pair "(" ")" :wrap "C-(") ;parens wrapping in evil mode
   (sp-pair "\"" "\"" :wrap "C-\"")
   (sp-pair "{" "}" :wrap "C-{")
@@ -194,7 +202,7 @@
   :after projectile
   :init
   (setq projectile-completion-system 'helm
-	projectile-switch-project-action 'helm-projectile)
+        projectile-switch-project-action 'helm-projectile)
   :config
   (helm-projectile-on))
 
@@ -217,7 +225,7 @@
     "tn" '(neotree-toggle :which-key "neotree toggle"))
   :init
   (setq neo-theme 'nerd
-	neo-smart-open t))
+        neo-smart-open t))
 
 (use-package centaur-tabs
   :ensure t
@@ -225,11 +233,11 @@
   :general
   (nmap
     "s-}" #'centaur-tabs-forward
-    "s-{" #'centaur-tabs-backward) 
+    "s-{" #'centaur-tabs-backward)
   :init
   (setq centaur-tabs-style "rounded"
-	centaur-tabs-set-modified-marker t
-	centaur-tabs-modified-marker "*")
+        centaur-tabs-set-modified-marker t
+        centaur-tabs-modified-marker "*")
   :config
   (centaur-tabs-mode t)
   (centaur-tabs-headline-match)
@@ -245,7 +253,7 @@
     "gg" '(magit-status :which-key "magit status"))
   :init
   (setq magit-last-seen-setup-instructions "1.4.0"
-	magit-display-buffer-function #'magit-display-buffer-fullframe-status-v1)
+        magit-display-buffer-function #'magit-display-buffer-fullframe-status-v1)
 
   :config
   (use-package evil-magit :ensure t))
