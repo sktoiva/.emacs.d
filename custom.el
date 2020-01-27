c9ba0e703d28ba2e5dd9881fb15db82054ea964d
sktoiva <sampo.toiva@gmail.com>
2020-01-27 21:24:24 +0200

    Update custom.el

--- custom.el
+++ custom.el
@@ -3,14 +3,13 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (cljr-helm clj-refactor cider-eval-sexp-fu avy delight evil-magit which-key use-package telephone-line rainbow-mode rainbow-delimiters nord-theme neotree magit helm-projectile helm-ag general exec-path-from-shell evil-smartparens evil-goggles company cider ag)))
  '(safe-local-variable-values
    (quote
     ((cider-default-cljs-repl . figwheel-main)
      (cider-figwheel-main-default-options . ":dev")))))
-
- '(package-selected-packages
-   (quote
-    (evil-magit which-key use-package telephone-line rainbow-mode rainbow-delimiters nord-theme neotree magit helm-projectile helm-ag general exec-path-from-shell evil-smartparens evil-goggles company cider centaur-tabs ag))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
